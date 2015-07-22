ce0bc2456939c44b107f5be38684eb71aca9dbca
drasnop <ponsard.antoine@gmail.com>
2015-07-22 14:51:16 -0700

    allow input from either an aggregate table (like before) or the trials themselves

--- parametric_between_subjects.R
+++ parametric_between_subjects.R
@@ -1,10 +1,24 @@
 source("util.R")
 
 # parametric data taken from each participant in the experiment (between-subjects)
-all_data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/2-24/aggregate-2-24.csv")
+data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/2-24/trials-2-24.csv")
+#data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/2-24/aggregate-2-24.csv")
+
+# remove practice trial
+data <- subset(data, block>0)
+
+# remove participants with major problems
+data <- subset(data, problems <=1)
+
+# keep only successful trials
+data <- subset(data, success == 1)
+# data <- subset(data, timeout == 0)
+
+# log-transform
+data$shortDuration <- log(data$shortDuration)
 
 util.printBigHeader("Running Parametric between-subjects Analysis on interface type");
-condition_results <- util.betweenSubjectsAnalysis(all_data, "id", "medianShortDuration", "interfaceType", "id")
+condition_results <- util.betweenSubjectsAnalysis(data, "id", "shortDuration", "interfaceType", "id")
 
 # util.printBigHeader("Running Parametric between-subjects Analysis on number of errors");
-# condition_results <- util.betweenSubjectsAnalysis(all_data, "id", "numSuccesses", "interfaceType", "id")
+# condition_results <- util.betweenSubjectsAnalysis(data, "id", "numSuccesses", "interfaceType", "id")
--- parametric_mixed_design.R
+++ parametric_mixed_design.R
@@ -1,8 +1,21 @@
 source("util.R")
 
 # parametric data taken from each participant in the experiment
-all_data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/1-20-pax/trials-1-20-pax.csv")
-columns <- c("interface") # meh
+# data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/2-24/trials-2-24.csv")
+data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/2-24/aggregate-2-24.csv")
 
-util.printBigHeader("Running Parametric Analysis on interface type (between-subjects) and trial number (within-subject)");
-condition_results <- util.mixedDesignAnalysis(all_data, columns, "id", "shortDuration", "interfaceType", "trialNumber", "id")
+# remove practice trial
+data <- subset(data, block>0)
+
+# remove problematic participants
+data <- subset(data, problems <=1)
+
+# keep only successful trials
+#data <- subset(data, success == 1)
+# data <- subset(data, timeout == 0)
+
+# log-transform
+#data$shortDuration <- log(data$shortDuration)
+
+util.printBigHeader("Running Parametric Analysis on interface type (between-subjects) and block (within-subject)");
+condition_results <- util.mixedDesignAnalysis(data, "id", "medianShortDuration", "interfaceType", "block", "id")
