3f6cdcf4095d94bf1000bdbbd0998b2f21a88118
drasnop <ponsard.antoine@gmail.com>
2015-07-27 18:50:41 -0700

    cleaner way to set up the analysis, using variable names

--- parametric_mixed_design.R
+++ parametric_mixed_design.R
@@ -1,21 +1,28 @@
 source("util.R")
 
 # parametric data taken from each participant in the experiment
-# data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/2-24/trials-2-24.csv")
-data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/2-24/aggregate-2-24.csv")
+data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/2-24/trials-2-24.csv")
+#data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/2-24/aggregate-2-24.csv")
+
+measure <- "shortDuration"
+between <- "interfaceType"
+within <- "hookInTutorial"
 
 # remove practice trial
 data <- subset(data, block>0)
+
+#data <- subset(data, (block == 1 & trialNumber >2) | (block == 2 & trialNumber >22))
 
 # remove problematic participants
 data <- subset(data, problems <=1)
 
 # keep only successful trials
-#data <- subset(data, success == 1)
+data <- subset(data, success == 1)
 # data <- subset(data, timeout == 0)
+#data <- subset(data, ghost != 1)
 
 # log-transform
-#data$shortDuration <- log(data$shortDuration)
+data[[measure]] <- log(1+data[[measure]])
 
-util.printBigHeader("Running Parametric Analysis on interface type (between-subjects) and block (within-subject)");
-condition_results <- util.mixedDesignAnalysis(data, "id", "medianShortDuration", "interfaceType", "block", "id")
+util.printBigHeader(paste0("Running Parametric Analysis for ", measure, " on ", between, " (between-subjects) and ", within," (within-subject)"));
+condition_results <- util.mixedDesignAnalysis(data, "id", measure, between, within, "id")
