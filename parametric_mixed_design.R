a083270c501114cd3b534ed9e7a3c5c2942b5711
drasnop <ponsard.antoine@gmail.com>
2015-08-26 13:34:34 -0700

    added assumptions tests and other minor changes

--- parametric_mixed_design.R
+++ parametric_mixed_design.R
@@ -1,20 +1,19 @@
 source("util.R")
-#suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(dplyr))
+#library(car)
+#library(afex)
 
 # parametric data taken from each participant in the experiment
 filename <- function(batch){
   return(paste0("C:/Users/Antoine/programmation/web/stencil-analysis/", batch, "/trials-", batch, ".csv"))
 }
-csv <- lapply(c("2-24", "3-24", "rect-15"), function(batch) read.csv(filename(batch)))
+csv <- lapply(c("2-24", "3-24", "rect-15", "4-12", "rect-2"), function(batch) read.csv(filename(batch)))
 data <- do.call(rbind, csv)
 
 measure <- "shortDuration"
 estimator <- median
-between <- "partition"
+between <- "interface"
 within <- "block"
-
-data$timeout <- as.logical(data$timeout)
-data$success <- as.logical(data$success)
 
 # remove practice trial
 data <- subset(data, block!=0)
@@ -22,12 +21,15 @@ data <- subset(data, block!=0)
 # remove problematic participants
 data <- subset(data, problems<=0)
 
-# outlier in Customization Mode
+# outlier in Customization Mode (one very slow, one with 14 errors, two who participated in the pilot, one with 18 errors)
 data <- subset(data, !(id %in% c("xqpi3r9n")))
+data <- subset(data, !(id %in% c("bicjgan9")))
+data <- subset(data, !(id %in% c("esfq3ee6", "l04et6xn")))
+data <- subset(data, !(id %in% c("yacy699g")))
 #data <- subset(data, !(id %in% c("0dksgr1h", "c7nb2rhh", "yj20fln6", "8cugv923" )))
 
 # outliers in control
-data <- subset(data, !(id %in% c("f5arveax", "n30x5hx4", "q7d654an")))
+#data <- subset(data, !(id %in% c("f5arveax", "n30x5hx4", "q7d654an")))
 
 # keep only successful trials
 #data <- subset(data, success)
@@ -36,7 +38,6 @@ data <- subset(data, !(id %in% c("f5arveax", "n30x5hx4", "q7d654an")))
 
 
 # log-transform
-#if(estimator == mean)
 if(is.numeric(data[[measure]]))
   data[[measure]] <- log(1+data[[measure]])
 
@@ -55,8 +56,12 @@ if(is.numeric(data[[measure]])){
 }
 
   
-# aggregate
+## aggregate
 collapsed <- aggregate(as.formula(paste(measure,"~ id +",within,"+",between)), data, estimator)
+util.printBigHeader(paste0("Running Parametric Analysis for ", measure, " on ", between, " (between-subjects) and ", within," (within-subject)"));
+
+
+## Assumptions
 
 # plot histograms to check normality
 hist(collapsed[[measure]], breaks=30)
@@ -64,12 +69,17 @@ hist(collapsed[[measure]], breaks=30)
 # plot kernel density plots to check normality
 plot(density(collapsed[[measure]]))
 
+# Homogeneity of variance test
+print(leveneTest(as.formula(paste(measure,"~",between)), collapsed))
+# non-parametric
+#fligner.test(as.formula(paste(measure,"~interaction(",within,",",between,")")), collapsed)
+
+# no need to test for sphericity if only two levels of within-subject factor
 
 
-# ANOVA
-util.printBigHeader(paste0("Running Parametric Analysis for ", measure, " on ", between, " (between-subjects) and ", within," (within-subject)"));
+## ANOVA
 results <- util.mixedDesignAnalysis(collapsed, "id", measure, between, within, "id")
-
+# model <- mixed(shortDuration ~ interface + block + interface:block + (1|id), collapsed) using package afex
 
 # boxplot the data to look for outliers in each cell
 #outliers <- boxplot(as.formula(paste(measure,"~",within,"+",between)), collapsed)$out
