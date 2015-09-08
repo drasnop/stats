e1b18d165c6f2706753144872203facb0112251c
drasnop <ponsard.antoine@gmail.com>
2015-09-07 22:24:27 -0700

    new scatterplots with 3 dimensions: numCAS, numAS, shortDuration

--- plots.R
+++ plots.R
@@ -15,10 +15,12 @@ theme_set(theme_miniBW())
 #data <- mutate(data, ASscore= correctAnchorHasBeenSelected/numSelectedHooks)
 data <- mutate(data, ASscore= ifelse(numSelectedHooks>1, .5, correctAnchorHasBeenSelected))
 SAS <- aggregate(ASscore~interface+block+id, data, mean)
+nAS <- aggregate(numSelectedHooks~interface+block+id, data, mean)
 CAS <- aggregate(correctAnchorHasBeenSelected~interface+block+id, data, sum)
 CAS <- mutate(CAS, controlStart= id %in% seq(1:6))
 comp <- merge(CAS, collapsed)
 comp <- merge(comp, SAS)
+comp <- merge(comp, nAS)
 
 
 #------ density Correct Anchor Selected ------#
@@ -48,8 +50,7 @@ tableauPalette <- c("#1F77B4", "#17BECF", "#FF7F0E", "#9467BD")
 DarkYlGnBu <- c("#7fcdbb", "#41b6c4", "#225ea8", "#000000")
 scatter <- ggplot(comp, aes(x=correctAnchorHasBeenSelected, y=shortDuration)) + scale_colour_manual(values=tableauPalette)
 
-#scatter <- scatter + geom_point(data=subset(comp, block==1), size=4, shape=21, aes(color=interface))
-#scatter <- scatter + geom_point(data=subset(comp, block==2), size=2, shape=19, aes(color=interface))
+#scatter <- scatter + geom_point(aes(size=block, shape=block, color=interface)) + scale_shape_manual(values = c(21, 19)) + scale_size_manual(values=c(3,2))
 #scatter <- scatter + geom_line(aes(group=id, color=interface))
 
 scatter <- scatter + geom_jitter(size=3, aes(color=interface))
@@ -59,14 +60,38 @@ scatter <- scatter + facet_grid(controlStart ~ block)
 #scatter <- scatter + facet_grid( ~ controlStart)
 
 
-#CairoWin()    # separate rendering window for antialiasing
+CairoWin()    # separate rendering window for antialiasing
 
 print(scatter + theme_bw())
 
 # save output to file
-#CairoPNG("C:/Users/Antoine/Dropbox/research/Experiment/figures/mturk/0.png",1920, 900)
+CairoPNG("C:/Users/Antoine/Dropbox/research/Experiment/figures/mturk/0.png",1600, 900)
 #CairoPNG("C:/Users/Antoine/Dropbox/research/Experiment/figures/lab/0.png",1800, 900)
 # wait a couple seconds before calling again print(scatter)
+
+# CAS vs numAS ~ interface + block
+scatter <- ggplot(comp, aes(x=numSelectedHooks, y=correctAnchorHasBeenSelected)) + scale_colour_manual(values=tableauPalette)
+
+#scatter <- scatter + geom_point(aes(size=block, shape=block, color=interface)) + scale_shape_manual(values = c(21, 19)) + scale_size_manual(values=c(3,2))
+#scatter <- scatter + geom_line(aes(group=id, color=interface))
+
+scatter <- scatter + geom_jitter(size=3, aes(color=interface))
+scatter <- scatter + geom_jitter(shape=21, aes(color=interface, size=-shortDuration))
+scatter <- scatter + facet_grid(~ block) + theme_bw()
+
+print(scatter)
+
+
+# duration & numAS vs CAS ~ interface + block
+scatter <- ggplot(comp, aes(x=correctAnchorHasBeenSelected, y=shortDuration)) + scale_colour_manual(values=tableauPalette)
+
+scatter <- scatter + geom_point(aes(size=numSelectedHooks, shape=block, color=interface)) + scale_shape_manual(values = c(21, 19))
+scatter <- scatter + geom_line(aes(group=id, color=interface))
+
+#scatter <- scatter + geom_point(shape=21, aes(color=interface, size=numSelectedHooks))
+#scatter <- scatter + facet_grid(~ block) + theme_bw()
+
+print(scatter)
 
 
 #------ comparative distributions : block ------#
