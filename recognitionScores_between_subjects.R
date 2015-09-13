ae54333c37bfceebb9208e9654f6abe2ed2dcec1
drasnop <ponsard.antoine@gmail.com>
2015-09-12 17:16:32 -0700

    updated recognition score ANOVA for all mturk data

--- recognitionScores_between_subjects.R
+++ recognitionScores_between_subjects.R
@@ -1,7 +1,8 @@
 source("util.R")
+source("data_manager.R")
 
-# parametric data taken from each participant in the experiment (between-subjects)
-data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/2-24/questionnaires-2-24.csv")
+data <- load.mturk("questionnaires")
+data <- removeProblemsAndOutliers(data)
 
 util.printBigHeader("Running Parametric between-subjects Analysis");
 condition_results <- util.betweenSubjectsAnalysis(data, "id", "tabsRecognitionScore", "interface", "id")
