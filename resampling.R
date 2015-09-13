92044cacce25f02fb04651afd560a4fc8117f71c
drasnop <ponsard.antoine@gmail.com>
2015-09-13 15:25:40 -0700

    refactoring: store all the results + display both p values and ges

--- resampling.R
+++ resampling.R
@@ -1,22 +1,33 @@
 n = 1000;
 
-pvalues <- sapply(1:n, function(x) {
-  ids <- sample(as.character(subset(collapsed, interface == "0" & block == "1")$id), 12)
-  sampled <<- subset(collapsed, id %in% ids | interface != "0")
+results <- lapply(1:n, function(x) {
+  ids <<- sample(as.character(subset(collapsed, interface == "Control" & block == "1")$id), 12)
+  sampled <<- subset(collapsed, id %in% ids | interface != "Control")
 
-  suppressWarnings(results <- eval(parse(text=paste0("ezANOVA(data=sampled, dv=", measure, ", wid=id", ", between=", between, ", within=", within, ", type=3)"))))
-  print(paste(x, results$ANOVA$p[1]))
+  suppressWarnings(result <- eval(parse(text=paste0("ezANOVA(data=sampled, dv=", measure, ", wid=id", ", between=", between, ", within=", within, ", type=3)"))))
+  print(paste(x, result$ANOVA$p[1]))
   
-  return(results$ANOVA$p[1]);
+  return(result$ANOVA);
 })
 
-# one summary of the data
+# print one summary of the data
 print(eval(parse(text=paste0("ezStats(data=sampled, dv=", measure, ", wid=id", ", between=", between, ", within=", within, ")"))))
 
-plot(density(pvalues))
-hist(pvalues, breaks=50)
+# print distributions of interesting parameters
+distribution <- function(parameter, index){
+  values <- sapply(results, function(ANOVA){
+    return(ANOVA[[parameter]][[index]])
+  })
+  
+  plot(density(values), main=paste0(parameter, '[', index, ']'))
+  #hist(values, breaks=50, main=paste0(parameter, '[', index, ']'))
+}
 
-#print(paste(length(ids), length(subset(sampled, interface == "0" & block == "1")$id)))
+distribution("p", 1)
+distribution("ges", 1)
+
+
+#print(paste(length(ids), length(subset(sampled, interface == "Control" & block == "1")$id)))
 
 #temp <- split(collapsed, collapsed$interface)
 #control <- split(temp$`0`, temp$`0`$block)
