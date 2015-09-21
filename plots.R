0238693c3041c6398925abf6b5fd78f2db5a3b87
drasnop <ponsard.antoine@gmail.com>
2015-09-20 17:22:41 -0700

    minor

--- plots.R
+++ plots.R
@@ -14,13 +14,20 @@ theme_set(theme_miniBW())
 
 #data <- mutate(data, ASscore= correctAnchorHasBeenSelected/numSelectedHooks)
 data <- mutate(data, ASscore= ifelse(numSelectedHooks>1, .5, correctAnchorHasBeenSelected))
+
 SAS <- aggregate(ASscore~interface+block+id, data, mean)
 nAS <- aggregate(numSelectedHooks~interface+block+id, data, mean)
 CAS <- aggregate(correctAnchorHasBeenSelected~interface+block+id, data, sum)
-CAS <- mutate(CAS, controlStart= id %in% seq(1:6))
+
+#SAS <- aggregate(ASscore~interface+id, data, mean)
+#nAS <- aggregate(numSelectedHooks~interface+id, data, mean))
+#CAS <- aggregate(correctAnchorHasBeenSelected~interface+id, data, mean)
+#comp <- merge(CAS, nAS)
+
 comp <- merge(CAS, collapsed)
 comp <- merge(comp, SAS)
 comp <- merge(comp, nAS)
+comp <- mutate(comp, controlStart= id %in% seq(1:6))
 
 
 #------ density Correct Anchor Selected ------#
@@ -41,7 +48,7 @@ par(mfrow=c(2,2))
 par(mfrow=c(1,1))
 
 plot(density(subset(CAS, interface!="Control")$correctAnchorHasBeenSelected), xlab="Num Correct Anchor Selected", main="")
-#hist(subset(CAS, interface!="Control")$correctAnchorHasBeenSelected, xlab="Num Correct Anchor Selected", main="", breaks=10)
+hist(subset(CAS, interface!="Control")$correctAnchorHasBeenSelected, xlab="Num Correct Anchor Selected", main="", breaks=20)
 
 
 #------ scatter plot Performance vs CAS ------#
@@ -49,11 +56,12 @@ plot(density(subset(CAS, interface!="Control")$correctAnchorHasBeenSelected), xl
 tableauPalette <- c("#1F77B4", "#17BECF", "#FF7F0E", "#9467BD")
 DarkYlGnBu <- c("#7fcdbb", "#41b6c4", "#225ea8", "#000000")
 scatter <- ggplot(comp, aes(x=correctAnchorHasBeenSelected, y=shortDuration)) + scale_colour_manual(values=tableauPalette)
+#scatter <- ggplot(comp, aes(x=numSelectedHooks, y=correctAnchorHasBeenSelected)) + scale_colour_manual(values=tableauPalette)
 
 #scatter <- scatter + geom_point(aes(size=block, shape=block, color=interface)) + scale_shape_manual(values = c(21, 19)) + scale_size_manual(values=c(3,2))
 #scatter <- scatter + geom_line(aes(group=id, color=interface))
 
-scatter <- scatter + geom_jitter(size=3, aes(color=interface))
+scatter <- scatter + geom_point(size=3, aes(color=interface, shape=id)) +  scale_shape_manual(values = c(0,1,2,15,19,17,0,1,2,15,19,17))
 
 #scatter <- scatter + facet_grid(~ block)
 scatter <- scatter + facet_grid(controlStart ~ block)
@@ -65,7 +73,7 @@ CairoWin()    # separate rendering window for antialiasing
 print(scatter + theme_bw())
 
 # save output to file
-CairoPNG("C:/Users/Antoine/Dropbox/research/Experiment/figures/mturk/0.png",1600, 900)
+CairoPNG("C:/Users/Antoine/Dropbox/research/Experiment/figures/mturk/0.png",1200, 900)
 #CairoPNG("C:/Users/Antoine/Dropbox/research/Experiment/figures/lab/0.png",1800, 900)
 # wait a couple seconds before calling again print(scatter)
 
@@ -76,6 +84,7 @@ scatter <- ggplot(comp, aes(x=numSelectedHooks, y=correctAnchorHasBeenSelected))
 #scatter <- scatter + geom_line(aes(group=id, color=interface))
 
 scatter <- scatter + geom_jitter(size=3, aes(color=interface))
+
 scatter <- scatter + geom_jitter(shape=21, aes(color=interface, size=-shortDuration))
 scatter <- scatter + facet_grid(~ block) + theme_bw()
 
@@ -99,6 +108,12 @@ print(scatter)
 distribution <- ggplot(subset(CAS, interface!= "Control"), aes(x=correctAnchorHasBeenSelected))
 distribution <- distribution + geom_density(trim=FALSE, aes(color=block)) + scale_color_manual(values=DarkYlGnBu)
 #distribution <- distribution + coord_cartesian(xlim = c(-5, 25)) 
+print(distribution)
+
+distribution <- ggplot(subset(CAS, interface!= "Control"), aes(x=correctAnchorHasBeenSelected))
+distribution <- distribution + geom_histogram(binwidth=2, color="black", fill="white")
+#distribution <- distribution + geom_density(trim=FALSE)
+#distribution <- distribution + facet_grid(~ block) + theme_bw()
 print(distribution)
 
 distribution <- ggplot(subset(SAS, interface!= "Control"), aes(x=ASscore))
